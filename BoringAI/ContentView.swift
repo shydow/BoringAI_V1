15dbd55b08f9bc58acf1ac53b13581a23f8f0200
shydow <shydow@me.com>
2019-12-12 10:42:35 +0800

    Initial Commit

---
+++ BoringAI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoringAI
+//
+//  Created by Shydow Lee on 2019/12/12.
+//  Copyright © 2019 Shydow Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
